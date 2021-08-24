3fe710f46e22bd88283ccef9f874a9673054ed5a
Andrew Mason <andrewmason@Andrews-MacBook-Pro.local>
2021-08-24 15:07:10 -0400

    Updated section-title styles, reduced section sizes to min-vh-75

--- src/components/SectionComponent.vue
+++ src/components/SectionComponent.vue
@@ -2,7 +2,7 @@
     <b-container fluid class="p-0">
 
         <!-- full width text section -->
-        <b-row v-if="level === 1" align-v="center" class="min-vh-100" :class="rowClass">
+        <b-row v-if="level === 1" align-v="center" class="min-vh-75 bg-light" :class="rowClass">
             <b-col lg="8" offset-lg="2" align-self="center" class="text-center p-5">
                 <h5 class="text-uppercase font-weight-bold" v-html="microTitle"></h5>
                 <h2 class="section-title" v-html="title"></h2>
@@ -12,15 +12,15 @@
         </b-row>
         
         <!-- full width img section -->
-        <b-row v-else-if="level === 2" align-v="center" class="min-vh-100">
+        <b-row v-else-if="level === 2" align-v="center" class="min-vh-75">
             <b-col lg="12" align-self="stretch" class="p-0">
-                <b-img v-if="img1" :src="require(`@/assets/img/${img1}`)" fluid class="img-full min-vh-100"></b-img>
-                <b-img v-else :src="img2" fluid class="img-full min-vh-100"></b-img>
+                <b-img v-if="img1" :src="require(`@/assets/img/${img1}`)" fluid class="img-full min-vh-50"></b-img>
+                <b-img v-else :src="img2" fluid class="img-full min-vh-50"></b-img>
             </b-col>
         </b-row>
 
         <!-- 50-50 text-img section -->
-        <b-row v-else-if="level === 3" align-v="center" class="min-vh-100 bg-light" :class="rowClass">
+        <b-row v-else-if="level === 3" align-v="center" class="min-vh-75 bg-light" :class="rowClass">
             <b-col lg="6" align-self="center" class="text-left p-5">
                 <h6 class="small text-uppercase" v-html="microTitle"></h6>
                 <h2 class="h1 font-weight-bold" v-html="title"></h2>
@@ -29,17 +29,17 @@
             </b-col>
             <b-col lg="6" align-self="stretch" class="p-0" :class="colClass">
                 <b-img v-if="img1" :src="require(`@/assets/img/${img1}`)" fluid 
-                class="img-full min-vh-100"
+                class="img-full min-vh-50"
                 :class="imgClass"></b-img>
-                <b-img v-else :src="img2" fluid class="img-full min-vh-100"></b-img>
+                <b-img v-else :src="img2" fluid class="img-full min-vh-50"></b-img>
             </b-col>
         </b-row>
         
         <!-- 50-50 img-text section -->
-        <b-row v-else-if="level === 4" align-v="center" class="min-vh-100 bg-linen" :class="rowClass">
+        <b-row v-else-if="level === 4" align-v="center" class="min-vh-75 bg-linen" :class="rowClass">
             <b-col lg="6" align-self="stretch" class="p-0">
-                <b-img v-if="img1" :src="require(`@/assets/img/${img1}`)" fluid class="img-full min-vh-100"></b-img>
-                <b-img v-else :src="img2" fluid class="img-full min-vh-100"></b-img>
+                <b-img v-if="img1" :src="require(`@/assets/img/${img1}`)" fluid class="img-full min-vh-50"></b-img>
+                <b-img v-else :src="img2" fluid class="img-full min-vh-50"></b-img>
             </b-col>
             <b-col lg="6" align-self="center" class="text-left p-5">
                 <h6 class="small text-uppercase" v-html="microTitle"></h6>
