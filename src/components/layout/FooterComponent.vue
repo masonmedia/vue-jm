402c11d628bc910c9b736d18bc28a6db34f4a675
Andrew Mason <andrewmason@Andrews-MacBook-Pro.local>
2021-08-26 09:26:08 -0400

    Update to model, added footer component

---
+++ src/components/layout/FooterComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="p-0">
+
+        <!-- full width text section -->
+        <b-row align-v="center" class="bg-dark">
+            <b-col lg="8" offset-lg="2" align-self="center" class="text-center p-5">
+                <p class="my-3" v-html="text"></p>
+            </b-col>
+        </b-row>
+        
+  </section>
+</template>
+
+<script>
+import data from '@/frontaid/frontaid.content.json'
+
+export default {
+    name: 'FooterComponent',
+    data() {
+        return {
+            data: data.page_1
+        }
+    },
+    props: {
+        microTitle: String,
+        title: String,
+        text: String,
+        btn: String,
+        btn1: String,
+        btn2: String,
+        img1: String,
+        img2: String,
+        rowClass: String,
+        colClass: String,
+        imgClass: String,
+    },
+}
+</script>
