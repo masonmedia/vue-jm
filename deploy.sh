fcb61ff2546208cd1abb9f0ef9571caeb0267c82
Andrew Mason <andrewmason@Andrews-MacBook-Pro.local>
2021-08-28 22:50:03 -0400

    Update

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:masonmedia/vue_jm.git master:gh-pages
+git push -f git@github.com:masonmedia/vue-jm.git master:gh-pages
 cd -
